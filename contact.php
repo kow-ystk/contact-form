bfe960da95bda61ab4c4ad2e8b2c40113c522f30
Koki Kamezono <kow1ag25@gmail.com>
2021-01-07 01:34:21 +0900

    contact-1 formの中身をざっくり作成

--- contact.php
+++ contact.php
@@ -7,7 +7,31 @@
   <body>
     <section>
       <h1>お問い合わせ</h1>
-      <form action="" method="">
+      <form method="post">
+          <div>
+            <input type="text" name="name" placeholder="お名前" value="">
+          </div>
+          <div>
+            <input type="email" name="email" placeholder="メールアドレス" value="">
+          </div>
+          <div>
+            <input type="email" name="email" placeholder="確認用メールアドレス" value="">
+          </div>
+          </div>
+          <div>
+            <input type="number" name="age" placeholder="年齢" value="">
+          </div>
+          </div>
+          <div>
+            <input type="radio" name="gender" placeholder="性別" value="">
+          </div>
+          <div>
+            <input type="text" name="subject" placeholder="件名" value="">
+          </div>
+          <div>
+            <textarea name="message" placeholder="本文"></textarea>
+          </div>
+          <button type="submit">送信</button>
       </form>
     </section>
   </body>
