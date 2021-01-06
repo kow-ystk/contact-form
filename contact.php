a5febca5f597203f997451f0663b12b4763fda58
Koki Kamezono <kow1ag25@gmail.com>
2021-01-07 01:14:51 +0900

    first commit

---
+++ contact.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="jp">
+  <head>
+      <meta charset="utf-8">
+      <title>お問い合わせフォーム</title>
+  </head>
+  <body>
+    <section>
+      <h1>お問い合わせ</h1>
+      <form action="" method="">
+      </form>
+    </section>
+  </body>
+</html>
