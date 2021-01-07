6387475da44f01c5a7b67de2d1b2a1ee70a0df7b
Koki-Kamezono <koki.kamezono@nijibox.co.jp>
2021-01-07 10:32:50 +0900

    contact-1 labelを追加。性別のラジオボタンを追加。

--- contact.php
+++ contact.php
@@ -7,28 +7,38 @@
   <body>
     <section>
       <h1>お問い合わせ</h1>
-      <form method="post">
+      <form method="post" name="contactForm">
           <div>
+            <label for="name">名前</label>
             <input type="text" name="name" placeholder="お名前" value="">
           </div>
           <div>
+            <label for="email">メールアドレス</label>
             <input type="email" name="email" placeholder="メールアドレス" value="">
           </div>
           <div>
+            <label for="email">確認用メールアドレス</label>
             <input type="email" name="email" placeholder="確認用メールアドレス" value="">
           </div>
           </div>
           <div>
-            <input type="number" name="age" placeholder="年齢" value="">
+            <label for="age">年齢</label>
+            <input type="number" name="age" placeholder="年齢を数字で入力" value="">
           </div>
           </div>
           <div>
-            <input type="radio" name="gender" placeholder="性別" value="">
+            <p>性別</p>
+            <input type="radio" name="gender" value="1" id="male">
+            <label for="male">男性</label>
+            <input type="radio" name="gender" value="2" id="female">
+            <label for="female">女性</label>
           </div>
           <div>
+            <label for="subject">件名</label>
             <input type="text" name="subject" placeholder="件名" value="">
           </div>
           <div>
+            <label for="message">本文</label>
             <textarea name="message" placeholder="本文"></textarea>
           </div>
           <button type="submit">送信</button>
